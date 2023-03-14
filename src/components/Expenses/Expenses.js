290cc0ca54b8dc84b31eb14573b7e6b8627359a5
summereuna <summereuna@gmail.com>
2023-03-15 02:08:46 +0900

    Create ExpensesFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,27 @@
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangedYearHandler = (selectedYear) => {
+    console.log("Expenses.js");
+    console.log("selectedYear", selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
+  console.log("filteredYear", filteredYear);
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        onChangeFilterYear={filterChangedYearHandler}
+        //양방향 바인딩: state select value로 넣기 위해 props으로 보내기
+        selected={filteredYear}
+      />
       <ExpenseItem
         id={props.items[0].id}
         title={props.items[0].title}
