c968bd4d777939a74789cb684decdf1341dbdf0e
summereuna <summereuna@gmail.com>
2023-03-23 15:59:07 +0900

    Add styled components props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,6 +6,8 @@ import styled from "styled-components";
 
 //JSX에서 컴포넌트로 사용할 것이기 때문에 대문자로 작성
 //스타일 컴포넌트 패키지 사용: JSX의 div 부분 대체
+
+//스타일컴포넌트에 전달되는 일부 props에 따라 스타일을 동적으로 바꿀 수 있다.
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
@@ -13,12 +15,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -28,15 +32,6 @@ const FormControl = styled.div`
     outline: none;
     background: #fad0ec;
     border-color: #8b005d;
-  }
-
-  &.invalid input {
-    border-color: red;
-    background-color: #ffd7d7;
-  }
-
-  &.invalid label {
-    color: red;
   }
 `;
 
@@ -69,7 +64,7 @@ const CourseInput = (props) => {
   //다만 유효한지 아닌지를 체크하여 유효하지 않다면 invalid를 추가하면 된다.
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid ? "invalid" : ""}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
@@ -79,10 +74,8 @@ const CourseInput = (props) => {
 };
 
 export default CourseInput;
-
 /*
-2. 동적 props
-이 파일에서만 사용할 수 있는 스타일컴포넌트를 원한다면 그 파일에서만 생성하면 된다.
-지금까지는 파일별로 하나의 컴포넌트를 가졌는데, 일반적으로 그대로 유지하면 좋다.
-하지만 파일에서, 다른 컴포넌트에도 사용되는 컴포넌트를 가지고 있다면 동일한 파일에서 그 두 컴포넌트를 가질 수도 있다.
+방법 3. styled 컴포넌트에 props 추가하여 백틱안에 props 사용하기
+백틱안에 props을 사용하면 스타일을 손쉽게 동적으로 바꿀 수 있다.
+`<FormControl invalid={!isValid}>`
 */
