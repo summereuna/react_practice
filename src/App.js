ca6e8c619af8fa39f60629f7a5370cce2aee163b
summereuna <summereuna@gmail.com>
2023-05-21 01:49:56 +0900

    Add loading

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
     const transformedMovies = data.results.map((movieData) => {
@@ -18,6 +21,8 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+
+    setIsLoading(false);
   };
 
   return (
@@ -26,7 +31,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -6,15 +6,14 @@ import classes from "./MoviesList.module.css";
 const MovieList = (props) => {
   return (
     <ul className={classes["movies-list"]}>
-      {props.movies &&
-        props.movies.map((movie) => (
-          <Movie
-            key={movie.id}
-            title={movie.title}
-            releaseDate={movie.releaseDate}
-            openingText={movie.openingText}
-          />
-        ))}
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
     </ul>
   );
 };
