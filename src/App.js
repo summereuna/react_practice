c45156f2c6d62dbcd9bf78791a6416088942da39
summereuna <summereuna@gmail.com>
2023-05-21 01:29:39 +0900

    Fix then to async await

--- src/App.js
+++ src/App.js
@@ -4,27 +4,20 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  //응답 결과 data 상태에 저장
   const [movies, setMovies] = useState();
 
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        //json 키 이름 변경해서 가져오기
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        //파싱 후 추출된 영화 data 트랜스폼한 후 movies 상태에 업데이트하기
-        setMovies(transformedMovies);
-      });
+  const fetchMoviesHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   };
 
   return (
